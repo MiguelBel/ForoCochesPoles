cbbd3a537583c4ba135b17c6c3c528c63e54ca78
miguel <miguel@ubuntu.(none)>
2014-09-26 16:26:15 +0200

    Tracker file added

---
+++ track_threads.rb
@@ -0,0 +1,11 @@
+require "pp"
+require "curb"
+require "nokogiri"
+
+require_relative 'lib/forocoches_api/url_constructor'
+require_relative 'lib/forocoches_api/thread_petitions'
+require_relative 'lib/forocoches_tracker/database'
+require_relative 'lib/forocoches_tracker/tracker'
+
+tracker = Tracker.new(50)
+tracker.doThePetitions
