afa2e25b251b501731fa9dca00fa71bcf8f7cb41
miguel <miguel@ubuntu.(none)>
2014-09-28 16:35:10 +0200

    Change last thread date to be a good one

--- lib/forocoches_web/controllers/user_interface_controllers.rb
+++ lib/forocoches_web/controllers/user_interface_controllers.rb
@@ -5,7 +5,7 @@ helpers do
     number_of_censored_threads = Poles.all(:poleman => "", :category => "",:fields => [:id]).count
     number_of_no_poleman_threads = Poles.all(:poleman => "", :category.not => "",:fields => [:id]).count
     number_of_general_threads = Poles.all(:category => "General", :fields => [:id]).count
-    last_thread_date = Poles.last(:category.not => "", :poleman.not => "" :fields => [:pole_time]).pole_time
+    last_thread_date = Poles.last(:category.not => "", :poleman.not => "", :fields => [:pole_time]).pole_time
     erb :main_page, :layout => nil, :locals => {:number_of_tracked_threads => number_of_tracked_threads, :number_of_censored_threads => number_of_censored_threads, :number_of_no_poleman_threads => number_of_no_poleman_threads, :number_of_general_threads => number_of_general_threads, :last_thread_date => last_thread_date}
   end
 
