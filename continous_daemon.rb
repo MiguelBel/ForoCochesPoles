66b885bf4219b5ed33f58e1332a89ffe9f5ead5c
miguel <miguel@ubuntu.(none)>
2014-10-19 21:04:44 +0200

    Continuos integration added pole_time on update

--- continous_daemon.rb
+++ continous_daemon.rb
@@ -13,7 +13,7 @@ Process.daemon(true)
 loop do
   pid = Process.fork do
     last_id = Poles.last.id_thread
-    
+
     ((last_id + 1)..(last_id + 11)).each_with_index do |thread_id, index|
       thread = FCThread.new(thread_id)
       tracker = Tracker.new
@@ -24,14 +24,14 @@ loop do
       end
     end
 
-    # adapter = DataMapper.repository(:default).adapter
-    # select = adapter.select("SELECT * FROM poles WHERE category = 'General' AND status = 'no_pole_yet' AND poleman IS NULL AND time > (#{Time.now.to_i - 30 * 60}) ORDER BY time ASC LIMIT 20;")
-    # select.each do |previous_thread|
-    #   thread = FCThread.new(previous_thread.id_thread)
-    #   if thread.poleman != false
-    #     adapter.execute("UPDATE poles SET status='', poleman='#{thread.poleman}', pole_time='#{thread.pole_time}' WHERE id_thread = #{previous_thread.id_thread}")
-    #   end
-    # end
+    adapter = DataMapper.repository(:default).adapter
+    select = adapter.select("SELECT * FROM poles WHERE category = 'General' AND status = 'no_pole_yet' AND poleman IS NULL AND time > (#{Time.now.to_i - 30 * 60}) ORDER BY time ASC LIMIT 20;")
+    select.each do |previous_thread|
+      thread = FCThread.new(previous_thread.id_thread)
+      if !thread.poleman.nil?
+        adapter.execute("UPDATE poles SET status='', poleman='#{thread.poleman}', pole_time='#{thread.pole_time}' WHERE id_thread = #{previous_thread.id_thread}")
+      end
+    end
   end
 
   Process.waitpid(pid)
